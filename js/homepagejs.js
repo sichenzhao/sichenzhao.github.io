1806d65239f8e87fd201e42ef03bdee128113eab
sichenzhao <szhao@imac343.student.cs.uwaterloo.ca>
2014-08-04 21:32:21 -0400

    javascript for home dir

--- js/homepagejs.js
+++ js/homepagejs.js
@@ -1,5 +1,18 @@
-document.getElementByid("home-directory-table").getElementBy
+var home-ths = document.getElementByid("home-directory-table").getElementByTagName("th");
 
 function mOver(obj){
-	obj.
+	if(home-ths.indexOf(obj)!=-1){
+		obj.style.opacity = 1.00;
+	}
+}
+
+function mOut(obj){
+	if(home-ths.indexOf(obj)!=-1){
+		obj.style.opacity = 0.50;
+	}
+}
+
+for( i=0; i<home-ths.length; i++){
+	home-ths[i].onmouseover=mOver(home-ths[i]);
+	home-ths[i].onmouseout=mOut(home-ths[i]);
 }
